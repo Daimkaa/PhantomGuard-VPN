5bbd54b9cf411a9265ac8ff57c40d20607670c39
mendeeislife <mendsaikhan1227@gmail.com>
2023-03-20 12:59:08 +0800

    Folder Structure

---
+++ lib/constants/constants.dart
@@ -0,0 +1,8 @@
+class Constants {
+  static const String url = 'https://api.gurvanshiidel.mn/v1';
+  static const int primaryColor = 0xFF0BAE60;
+  static const int backgroundColor = 0xFFf8f9f9;
+
+  // Button Colors
+  static const int dangerColor = 0xFFFF4949;
+}
